a5f02a57ab55ac344c1e9dd26af6635c9f0fd419
Joel <joelcastagnani@hotmail.com>
2024-07-31 00:47:41 -0300

    break clase 10

--- src/pages/checkout/Checkout.jsx
+++ src/pages/checkout/Checkout.jsx
@@ -1,15 +1,55 @@
+import { useState } from "react";
+
 const Checkout = () => {
+  const [nombre, setNombre] = useState("");
+  const [email, setEmail] = useState("");
+  const [telefono, setTelefono] = useState("");
+
   const envioDeFormulario = (event) => {
     event.preventDefault();
-    console.log("Se envio el formulario");
+    console.log("Le mandamos toda la info al server");
+
+    console.log(nombre);
+    console.log(email);
+    console.log(telefono);
+  };
+
+  const capturarNombre = (event) => {
+    setNombre(event.target.value);
+  };
+  const capturarEmail = (event) => {
+    setEmail(event.target.value);
+  };
+  const capturarTelefono = (event) => {
+    setTelefono(event.target.value);
   };
 
   return (
     <div>
-      <h1>Aca va el formulario</h1>;
+      <h1>Aca va el formulario</h1>
       <form onSubmit={envioDeFormulario}>
-        <input type="text" placeholder="Ingresa tu nombre" />
-        <button type="submit">Enviar</button>
+        <input
+          type="text"
+          placeholder="Ingresa tu nombre"
+          onChange={capturarNombre}
+          name="nombre"
+        />
+
+        <input
+          type="text"
+          placeholder="Ingresa tu email"
+          onChange={capturarEmail}
+          name="email"
+        />
+        <input
+          type="text"
+          placeholder="Ingresa tu telefono"
+          onChange={capturarTelefono}
+          name="telefono"
+        />
+
+        <button>Enviar</button>
+        <button type="button">Cancelar</button>
       </form>
     </div>
   );
